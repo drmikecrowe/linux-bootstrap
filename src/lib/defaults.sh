62e2204d9a9d6076ff6e26d774c9320ba23c7616
Mike Crowe <drmikecrowe@gmail.com>
2020-04-04 06:02:13 -0400

    Removing my specific items

--- src/lib/defaults.sh
+++ src/lib/defaults.sh
@@ -5,8 +5,6 @@ function install_base_packages() {
     # NOTE: Update this with the latest tool added to the list
     if [ "$(which augtool)" != "" ]; then
         sudo apt update
-        sudo apt install -y apt-transport-https curl autojump bash-completion build-essential ca-certificates cifs-utils comprez \
-        direnv dselect gawk gdebi git jq mc mysql-client net-tools p7zip-full sshfs tmux tmux-plugin-manager vim-nox virtualenv \
-        vpnc-scripts yadm aptitude augeas-tools fonts-powerline libffi-dev
+        sudo apt install -y apt-transport-https curl autojump bash-completion build-essential 
     fi
 }
