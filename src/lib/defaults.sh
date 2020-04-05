b681e69a98e608f7f64a302c5291012b75ad0c7b
Mike Crowe <drmikecrowe@gmail.com>
2020-04-05 08:36:48 -0400

    Fixing installed base package test

--- src/lib/defaults.sh
+++ src/lib/defaults.sh
@@ -3,9 +3,9 @@
 
 function install_base_packages() {
     # NOTE: Update this with the latest tool added to the list
-    if [ "$(which augtool)" != "" ]; then
+    if [ "$(which augtool)" == "" ]; then
         sudo apt update
-        sudo apt install -y apt-transport-https curl autojump bash-completion build-essential 
+        sudo apt install -y apt-transport-https curl autojump bash-completion build-essential augeas-tools
     fi
 
     set +e
