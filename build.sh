458fd8e1b7d0168bea2adcb42195f925a0879335
Mike Crowe <drmikecrowe@gmail.com>
2022-09-25 18:34:46 -0400

    removing oid

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p target
-rm target/bootstrap*
-bashing uberbash 
-sed -i 's/^ *EOF$/EOF/g' ./target/bootstrap-*.sh 
-cp ./target/bootstrap* ./target/bootstrap.sh 
-REMOTE="$(git remote -v | grep -E 'origin.*fetch' | sed 's/^.*https:.*github.com.//' | sed 's/\.git .*//')"
-sed -i "s@raw.githubusercontent.com/.*@raw.githubusercontent.com/$REMOTE/master/target/bootstrap.sh@g" README.md
